f114398a3372372c8273aa4636b50e88d184300c
sergei khvalko <falko.serega2016@yandex.ru>
2024-11-07 22:37:49 +0300

    feat: add BottomNav

--- app.vue
+++ app.vue
@@ -1,11 +1,37 @@
 <template>
-  <!-- <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div> -->
-  <NuxtLayout>
-    <v-app>
+  <v-app>
+    <NuxtLoadingIndicator
+      color="repeating-linear-gradient(to right,var(--c-primary) 0%,var(--c-primary-active) 100%)"
+    />
+
+    <v-app-bar>
+      <v-app-bar-title>SCOREBOARD24</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
       <NuxtPage />
-    </v-app>
-  </NuxtLayout>
+    </v-main>
+
+    <BottomNav :pages="pages" class="hidden-md-and-up" />
+  </v-app>
 </template>
+
+<script setup>
+const pages = [
+  {
+    title: "Stats",
+    icon: "mdi-chart-bar",
+    to: "/stats",
+  },
+  {
+    title: "Leagues",
+    icon: "mdi-soccer",
+    to: "/leagues",
+  },
+  {
+    title: "Matches",
+    icon: "mdi-soccer-field",
+    to: "/matches",
+  },
+];
+</script>
---
+++ components/BottomNav.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-bottom-navigation color="indigo" grow height="66">
+    <v-btn v-for="item in pages" :key="item.to" :to="item.to">
+      <v-icon>{{ item.icon }}</v-icon>
+      {{ item.title }}
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script setup>
+const props = defineProps({
+  pages: {
+    type: Array,
+    required: true,
+    default: [],
+  },
+});
+</script>
